b912d525687fca9008afba744deb8f602253ad7f
Caleb Trachte <cetrachte@hotmail.com>
2020-06-24 15:23:52 -0500

    updated code to handle invalid entries, exit logic

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,20 @@ use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
+
 fn main() {
-    println!("Guess a number between 1 and 100!");
+    println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
-
     loop {
-        println!("Please input your guess: ");
+        println!("Enter a number between 1 and 100.");
+
+        let mut guess = String::new();
 
         io::stdin()
             .read_line(&mut guess)
-            .expect("Failed to read your input!");
+            .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
@@ -22,16 +23,14 @@ fn main() {
         };
 
         println!("You guessed: {}", guess);
-    
+
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("{} is too small!", guess),
-            Ordering::Greater => println!("{} is too big!", guess),
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("{} is correct! You win!", guess),
+                println!("You win!");
                 break;
             }
         }
-
     }
-
 }
