9e660673e8ee69f47f97fada8d059fd01c70904a
Caleb Trachte <cetrachte@hotmail.com>
2020-06-24 14:40:50 -0500

    update verbiage, add second guess

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,20 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    println!("Please input your first guess: ");
 
-    let mut guess = String::new();
+    let mut guess1 = String::new();
+    let mut guess2 = String::new();
 
     io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        .read_line(&mut guess1)
+        .expect("Failed to read your input!");
 
-    println!("You guessed: {}", guess);
+    println!("Please input your second guess: ");
+
+    io::stdin()
+        .read_line(&mut guess2)
+        .expect("Failed to read your input!");
+
+    println!("You first guessed: {} then guessed {}", guess1, guess2);
 }
